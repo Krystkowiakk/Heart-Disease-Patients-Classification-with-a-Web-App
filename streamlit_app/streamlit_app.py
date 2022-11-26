6818f2bc0b7be0edb8741d3ff111e8b6f805f223
Krystian Krystkowiak <krystkowiakk@gmail.com>
2022-11-26 19:48:51 +0100

    Add files via upload

--- streamlit_app/streamlit_app.py
+++ streamlit_app/streamlit_app.py
@@ -27,7 +27,7 @@ if show_data:
 #st.bar_chart(hist_values)
 
 st.subheader('Target Distibution')
-st.caption('The dataset is unbalaced as we there is more healthy people than ones having heart desease.')
+st.caption('The dataset is unbalaced as we there is more healthy people than ones having heart desease.') #better as pieplot?
 
 fig = plt.figure(figsize=(10, 4))
 graph = sns.countplot(x='HeartDisease', data=data)
@@ -41,7 +41,7 @@ st.caption('How heart disease is releated to deferent features from dataset?')
 
 feature = st.selectbox(
     'Choose:',
-    ('Smoking', 'AlcoholDrinking', 'Stroke', 'DiffWalking', 'Sex', 'PhysicalActivity', 'Asthma', 'KidneyDisease', 'SkinCancer'))
+    ('Smoking', 'AlcoholDrinking', 'Stroke', 'DiffWalking', 'Sex', 'PhysicalActivity', 'Asthma', 'KidneyDisease', 'SkinCancer', 'Diabetic', 'PhysicalActivity', 'GenHealth', 'Race'))
 fig, ax1 = plt.subplots(figsize=(10, 4))
 graph = sns.countplot(ax=ax1,x = feature , data = data, hue='HeartDisease')
 #graph.set_xticklabels(graph.get_xticklabels(),rotation=90)
@@ -50,50 +50,40 @@ for p in graph.patches:
     graph.text(p.get_x()+p.get_width()/2., height + 0.1,height ,ha="center")
 st.pyplot(fig)
 
-st.subheader('Heart Disease vs Age')
-st.caption('And how heart disease is releated to age?')
+st.subheader('Heart Disease vs Age & Lifestyle') #change to diferent plot, keeep age, alco and smoke as percentage
+st.caption('And how heart disease is releated to age and lifestyle?')
 show_numbers = st.checkbox('Show numbers', value=False)  #move that switch on side
-# add buttons for smokers or drinkers only
 fig, ax1 = plt.subplots(figsize=(10, 4))
-graph = sns.countplot(ax=ax1,x = 'AgeCategory' , data = data, hue='HeartDisease')
+
+show_smokers = st.radio(
+    "How smoking influence the Heart Disease? Show:",
+    ('Both', 'Smoking', 'No Smoking'))
+
+show_alcohol = st.radio(
+    "How alcohol drinking influence the Heart Disease? Show:",
+    ('Both', 'Alcohol Drinking', 'No Alcohol Drinking'))    
+
+if show_smokers == 'Smoking':
+    data_filtered = data[data['Smoking']=='Yes']
+elif show_smokers == 'No Smoking':
+    data_filtered = data[data['Smoking']=='No']
+else:
+    data_filtered = data
+
+if show_alcohol == 'Alcohol Drinking':
+    data_filtered = data_filtered[data_filtered['AlcoholDrinking']=='Yes']
+elif show_alcohol == 'No Alcohol Drinking':
+    data_filtered = data_filtered[data_filtered['AlcoholDrinking']=='No']
+else:
+    data_filtered = data_filtered
+
+graph = sns.countplot(ax=ax1,x = 'AgeCategory' , data = data_filtered, hue='HeartDisease', order=['18-24', '25-29', '30-34', '35-39','40-44', '45-49', '50-54', '55-59', '60-64','65-69', '70-74', '75-79', '80 or older'])
 if show_numbers:
     for p in graph.patches:
         height = int(p.get_height())
         graph.text(p.get_x()+p.get_width()/2., height + 0.1,height ,ha="center")
 st.pyplot(fig)
 
-#categorical_features=['Race', 'Diabetic', 'PhysicalActivity', 'GenHealth']
 
 
-
-# PART 3 - Seattle House Prices Table
-
-
-# st.write(
-# '''
-# ## Seattle House Prices
-# We can import data into our Streamlit app using pandas `read_csv` then display the resulting dataframe with `st.dataframe()`.
-
-# ''')
-
-
-#data = data.rename(columns={'LATITUDE': 'lat', 'LONGITUDE': 'lon'})
-
-
-
-
-# ## PART 5 - Mapping and Filtering Data
-# #
-# st.write(
-# '''
-# ## Mapping and Filtering Data
-# We can also use Streamlit's built in mapping functionality.
-# Furthermore, we can use a slider to filter for houses within a particular price range.
-# '''
-# )
-
-# price_input = st.slider('House Price Filter', int(data['PRICE'].min()), int(data['PRICE'].max()), 500000 )
-
-# price_filter = data['PRICE'] < price_input
-# st.map(data.loc[price_filter, ['lat', 'lon']])
 
