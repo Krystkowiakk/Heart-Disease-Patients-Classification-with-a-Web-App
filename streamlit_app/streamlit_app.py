e345468d6f0bbb000bbe6221fdf11c42c8aade06
Krystian Krystkowiak <krystkowiakk@gmail.com>
2022-12-09 21:46:33 +0100

    Add files via upload

--- streamlit_app/streamlit_app.py
+++ streamlit_app/streamlit_app.py
@@ -11,16 +11,26 @@ from sklearn.preprocessing import StandardScaler
 data = pd.read_csv('streamlit_app/out.csv')
 #st.text('Loading...Done')
 
+def dum_gen(col, lis):
+    for l in lis:
+        if df_check[col][0] == l:
+            df_check[l] = 1
+        else:
+            df_check[l] = 0
 
 with st.sidebar.form("my_form"):
    st.write("Check if you should visit the doctor!")
    Sex = st.selectbox("Sex", ("Female", "Male"))
-   AgeCategory = st.selectbox("Age Category", ('18-24', '25-29', '30-34', '35-39','40-44', '45-49', '50-54', '55-59', '60-64','65-69', '70-74', '75-79', '80 or older'))
-   Race = st.selectbox("Race", ('White', 'Asian', 'Hispanic', 'Black', 'American Indian/Alaskan Native', 'Other'))
-   f_height = st.slider('Height? (kg)', 50.0, 230.0, (160.0), step=1.0) #check standard measures
-   f_weight = st.slider('Weight? (cm)', 35.0, 200.0, (65.0), step=1.0)  #check standard measures
+   AgeCategory = st.selectbox("Age Category", ('AgeCategory_18-24', 'AgeCategory_25-29',
+       'AgeCategory_30-34', 'AgeCategory_35-39', 'AgeCategory_40-44',
+       'AgeCategory_45-49', 'AgeCategory_50-54', 'AgeCategory_55-59',
+       'AgeCategory_60-64', 'AgeCategory_65-69', 'AgeCategory_70-74',
+       'AgeCategory_75-79', 'AgeCategory_80 or older')) #names to be fixed
+   Race = st.selectbox("Race", ('Race_Hispanic', 'Race_White', 'Race_Black', 'Race_American Indian/Alaskan Native', 'Race_Asian',  'Race_Other', ))
+   f_height = st.slider('Height? (cm)', 50.0, 230.0, (160.0), step=1.0) #check standard measures
+   f_weight = st.slider('Weight? (kg)', 35.0, 200.0, (65.0), step=1.0)  #check standard measures
    BMI = 10000*f_weight/(f_height)**2
-   GenHealth = st.selectbox("General Health", ('Excellent', 'Very good', 'Good', 'Fair', 'Poor'))
+   GenHealth = st.selectbox("General Health", ('GenHealth_Excellent','GenHealth_Very good', 'GenHealth_Good','GenHealth_Fair', 'GenHealth_Poor' ))
    PhysicalActivity = st.selectbox("Phisical Activity", ("Yes", "No"))
    SleepTime = st.slider('Sleep Time?', 1.0, 24.0, (7.0), step=1.0)  #check standard measures
    Smoking = int(st.checkbox('Smoking'))
@@ -31,7 +41,7 @@ with st.sidebar.form("my_form"):
    Asthma = int(st.checkbox('Asthma'))
    KidneyDisease = int(st.checkbox('Kidney Disease'))
    SkinCancer = int(st.checkbox('Skin Cancer'))
-   Diabetic = st.selectbox("Diabetic", options=(i for i in data.Diabetic.unique()))
+   Diabetic = st.selectbox("Diabetic", ('Diabetic_No', 'Diabetic_No, borderline diabetes', 'Diabetic_Yes', 'Diabetic_Yes (during pregnancy)'))
    submitted = st.form_submit_button("Submit")
    if submitted:
         input_data = {
@@ -46,14 +56,20 @@ with st.sidebar.form("my_form"):
             'Asthma': [Asthma],
             'KidneyDisease': [KidneyDisease],
             'SkinCancer': [SkinCancer],
-#            'AgeCategory': [AgeCategory],
-#            'Race': [Race],
-#            'Diabetic': [Diabetic],
-#            'GenHealth': [GenHealth]
+            'AgeCategory': [AgeCategory],
+            'Race': [Race],
+            'Diabetic': [Diabetic],
+            'GenHealth': [GenHealth]
         }
         df_check = pd.DataFrame(data = input_data)
+        dum_gen('AgeCategory', ['AgeCategory_18-24', 'AgeCategory_25-29', 'AgeCategory_30-34', 'AgeCategory_35-39', 'AgeCategory_40-44', 'AgeCategory_45-49', 'AgeCategory_50-54', 'AgeCategory_55-59', 'AgeCategory_60-64', 'AgeCategory_65-69', 'AgeCategory_70-74', 'AgeCategory_75-79', 'AgeCategory_80 or older'])
+        dum_gen('Race', ['Race_American Indian/Alaskan Native', 'Race_Asian', 'Race_Black', 'Race_Hispanic', 'Race_Other', 'Race_White'])
+        dum_gen('Diabetic', ['Diabetic_No', 'Diabetic_No, borderline diabetes', 'Diabetic_Yes', 'Diabetic_Yes (during pregnancy)'])
+        dum_gen('GenHealth',['GenHealth_Excellent','GenHealth_Fair', 'GenHealth_Good', 'GenHealth_Poor', 'GenHealth_Very good'])
+        df_check = df_check.drop(columns=['AgeCategory','Race','Diabetic', 'GenHealth'])
         df_check =  df_check.replace({'Yes':1, 'No':0, 'Female':1,'Male':0 })
 #        df_check = pd.get_dummies(df_check, columns=['AgeCategory','Race','Diabetic', 'GenHealth'], drop_first=True)
+#        st.dataframe(df_check)
         scalar = pickle.load(open('streamlit_app/scaled.pkl', 'rb'))
         df_check_scal = scalar.transform(df_check)
 #        st.dataframe(df_check_scal)
