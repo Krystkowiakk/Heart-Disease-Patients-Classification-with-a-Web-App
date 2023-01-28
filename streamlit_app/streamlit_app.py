5f515380563b8e0ce5cc73da726be1887a4ba93f
Krystian Krystkowiak <krystkowiakk@gmail.com>
2023-01-28 19:18:23 +0100

    Add files via upload

--- streamlit_app/streamlit_app.py
+++ streamlit_app/streamlit_app.py
@@ -90,35 +90,18 @@ show_data = st.checkbox('Show Raw Data & Target Distribution', value=False)
 
 
 
-if show_data:
-    st.subheader('Raw Data')
-    st.caption('The dataset originally comes from the CDC and is a major part of the Behavioural Risk Factor Surveillance System (BRFSS), which conducts annual telephone surveys to gather data on the health status of U.S. residents. BRFSS completes more than 400,000 adult interviews each year, making it the largest continuously conducted health survey system in the world.". The most recent dataset (as of February 15, 2022) includes data from 2020.')
-    st.dataframe(data.drop(columns=['Unnamed: 0']))
-    st.subheader('Target Distibution')
-    st.caption('The dataset is unbalaced as we there is more healthy people than ones having heart desease.') #better as pieplot?
-
-    #fig = plt.figure(figsize=(10, 4))
-    #graph = sns.countplot(x='HeartDisease', data=data)
-    #for p in graph.patches:
-    #    height = int(p.get_height())
-    #    graph.text(p.get_x()+p.get_width()/2., height + 0.1,height ,ha="center")
-    #st.pyplot(fig)
-
-    import matplotlib.ticker as mtick
-    s = data['HeartDisease'].value_counts(normalize=True, sort=False).mul(100)
-    fig2 = plt.figure(figsize = (10,4))
-    ax = sns.barplot(x=s.index, y=s)
-    ax.set(ylabel="", xlabel='HeartDisease')
-    #ax.yaxis.set_major_formatter(mtick.PercentFormatter())
-    ax.set(yticklabels=[])
-    for i, p in enumerate(ax.patches):
-        percentage = '{:.1f}%'.format(s[i])
-        x = p.get_x() + 0.4
-        y = p.get_height()
-        ax.annotate(percentage, (x, y), ha='center')
-    st.pyplot(fig2)
-#hist_values = np.histogram(data['HeartDisease'], bins = 2)[0]
-#st.bar_chart(hist_values)
+import matplotlib.ticker as mtick
+s = data['HeartDisease'].value_counts(normalize=True, sort=False).mul(100)
+fig = plt.figure(figsize = (10,4))
+ax = sns.barplot(x=s.index, y=s)
+ax.set(ylabel="", xlabel='HeartDisease')
+ax.set(yticklabels=[])
+for i, p in enumerate(ax.patches):
+    percentage = '{:.1f}%'.format(s[i])
+    x = p.get_x() + 0.4
+    y = p.get_height()
+    ax.annotate(percentage, (x, y), ha='center')
+st.pyplot(fig)
 
 
 
